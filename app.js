e46c91cc8f0dfd848867aaec391f7dd98ebd3cde
mahaveergurjar <mahaveergurjar.work@gmail.com>
2024-09-03 11:46:26 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
+const cors = require('cors')
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -37,10 +38,13 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+
 
 const store = MongoStore.create({
   mongoUrl: dbURL,
