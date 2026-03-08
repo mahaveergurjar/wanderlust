de89d387a961ec4bb67fe3ca780b4f50c15b2784
mahaveergurjar <mahaveergurjar.work@gmail.com>
2026-03-08 21:01:10 +0530

    feat: Configure Express to trust the first proxy for deployment environments.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const cors = require("cors");
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1); // Trust first proxy (Render/Heroku/Vercel)
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
